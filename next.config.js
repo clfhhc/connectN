8ab6086db76208abe41ccbf12e511dec291d02fc
clfhhc <david.chen@minted.com>
2019-06-30 23:07:31 -0700

    added next config for next-offline

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 // next.config.js
 /* eslint-disable @typescript-eslint/no-var-requires */
+const withOffline = require('next-offline');
 const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 /* eslint-enable @typescript-eslint/no-var-requires */
@@ -7,15 +8,57 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const isProd = process.env.NODE_ENV === 'production';
 const prodAssetPrefix = '/connectN';
 
-module.exports = withTypescript({
-  webpack(config, options) {
-    // Do not run type checking twice:
-    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin());
+module.exports = withOffline(
+  withTypescript({
+    webpack(config, options) {
+      // Do not run type checking twice:
+      if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin());
 
-    return config;
-  },
-  assetPrefix: isProd ? prodAssetPrefix : '',
-  publicRuntimeConfig: {
-    linkPrefix: isProd ? prodAssetPrefix : '',
-  },
-});
+      return config;
+    },
+
+    // service-worker settings
+    generateInDevMode: true,
+    registerSwPrefix: '/static',
+    workboxOpts: {
+      swDest: 'static/service-worker.js',
+      globPatterns: ['app/static/**/*'],
+      globDirectory: '.',
+      modifyUrlPrefix: {
+        app: isProd ? prodAssetPrefix : '',
+      },
+      runtimeCaching: [
+        // {
+        //   urlPattern: /^https?.*/,
+        //   handler: 'NetworkFirst',
+        //   options: {
+        //     cacheName: 'https-calls',
+        //     networkTimeoutSeconds: 15,
+        //     expiration: {
+        //       maxEntries: 150,
+        //       maxAgeSeconds: 30 * 24 * 60 * 60,
+        //     },
+        //     cacheableResponse: {
+        //       statuses: [0, 200],
+        //     },
+        //   },
+        // },
+        {
+          urlPattern: /^https?.*/,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'offlineCache',
+            expiration: {
+              maxEntries: 200,
+            },
+          },
+        },
+      ],
+    },
+
+    assetPrefix: isProd ? prodAssetPrefix : '',
+    publicRuntimeConfig: {
+      linkPrefix: isProd ? prodAssetPrefix : '',
+    },
+  })
+);
