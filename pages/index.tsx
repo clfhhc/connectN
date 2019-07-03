1e042efe5f888ea0cfca39cbee593738af99266c
clfhhc <david.chen@minted.com>
2019-07-03 00:20:22 -0700

    test different config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NextFC, NextContext } from 'next';
-import Head from '../src/components/Head';
 import ManifestHead from '../src/components/Head/ManifestHead';
 import Link from '../src/components/Link';
 import ConnectN from '../src/components/ConnectN';
@@ -13,11 +12,6 @@ const Page: NextFC<{}, {}, NextContext> = () => (
       hrefPage="/"
       hrefManifest="/static/mainfest/mainfest.json"
     />
-    <Head title="Connect N" themeColor="white">
-      <Link href="/" passHref>
-        <link rel="canonical" />
-      </Link>
-    </Head>
     <ConnectN />
     <p>
       <Link href="/about">
